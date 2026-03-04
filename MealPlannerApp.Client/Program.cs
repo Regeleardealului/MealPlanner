b31e25d5f2004d37742e39d86a344b222bf1ecad
RegeleArdealului <152423334+Regeleardealului@users.noreply.github.com>
2026-03-04 21:46:24 +0100

    Update Program.cs

--- MealPlannerApp.Client/Program.cs
+++ MealPlannerApp.Client/Program.cs
@@ -9,24 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-// --- Szolgáltatások regisztrálása ---
-
-// 1. MudBlazor szolgáltatások
 builder.Services.AddMudServices();
-
-// 2. LocalStorage (a tokenek tárolásához a böngészõben)
 builder.Services.AddBlazoredLocalStorage();
-
-// 3. Azonosítási szolgáltatások bekapcsolása
 builder.Services.AddAuthorizationCore();
-
-// 4. Saját AuthenticationStateProvider regisztrálása
-// Itt mondjuk meg a Blazornak, hogy a mi osztályunkat használja az állapot figyelésére
 builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<ApiAuthenticationStateProvider>());
-
-// 5. HttpClient beállítása
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
